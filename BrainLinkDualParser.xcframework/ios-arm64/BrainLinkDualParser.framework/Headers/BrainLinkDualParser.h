60e1aff08ef82dcc37fcd964d7c9ec766038be2b
Macrotellect <58502192+Macrotellect@users.noreply.github.com>
2025-03-05 14:30:59 +0800

    commit framework

---
+++ BrainLinkDualParser.xcframework/ios-arm64/BrainLinkDualParser.framework/Headers/BrainLinkDualParser.h
@@ -0,0 +1,16 @@
+//
+//  BrainLinkDualParser.h
+//  BrainLinkDualParser
+//
+//  Created by Macrotellect on 2025/1/2.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BrainLinkDualParser.
+FOUNDATION_EXPORT double BrainLinkDualParserVersionNumber;
+
+//! Project version string for BrainLinkDualParser.
+FOUNDATION_EXPORT const unsigned char BrainLinkDualParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BrainLinkDualParser/PublicHeader.h>
